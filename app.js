b74a7bfbfaf86081fdc7c494ca78aa3933981788
Nikola Gazdic <nikola.gazdic90@gmail.com>
2022-08-12 16:46:24 -0400

    Fixed Services section

--- app.js
+++ app.js
@@ -7,4 +7,21 @@ $(document).ready(function () {
 			$('.navbar').removeClass('sticky');
 		}
 	});
+
+	//For interactive typing of "Im Developer"
+	var typed = new Typed('.typing-1', {
+		strings: ['Developer', 'Freelancer', 'RockClimber'],
+
+		typeSpeed: 80,
+		backSpeed: 60,
+		loop: true,
+	});
+
+	var typed = new Typed('.typing-2', {
+		strings: ['Developer', 'Freelancer', 'RockClimber'],
+
+		typeSpeed: 80,
+		backSpeed: 60,
+		loop: true,
+	});
 });
